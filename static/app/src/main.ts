8d5c62960e09730239d06ef52547dc77fc3d126c
rst2071 <roman.863@live.com>
2022-11-18 17:56:57 +0100

    output as webcomponent

--- static/app/src/main.ts
+++ static/app/src/main.ts
@@ -1,12 +1,22 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { createCustomElement } from '@angular/elements';
+import { createApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// get a hand on the `ApplicationRef` to access its injector
+createApplication({ providers: [] }).then((appRef) => {
+  // create a constructor of a custom element
+  const buchung = createCustomElement(
+    AppComponent, // component for Angular element
+    { injector: appRef.injector } // used to inject the component to the DOM
+  );
+
+  // register in a browser
+  customElements.define('ifge-buchung', buchung);
+});
