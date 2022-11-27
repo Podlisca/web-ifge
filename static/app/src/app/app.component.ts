2c230ce24007c43e28017da96ebf9eac28e682d8
rst2071 <roman.863@live.com>
2022-11-27 23:23:24 +0100

    Formular and animation

--- static/app/src/app/app.component.ts
+++ static/app/src/app/app.component.ts
@@ -1,12 +1,57 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation: ViewEncapsulation.ShadowDom
+  encapsulation: ViewEncapsulation.ShadowDom,
+  imports: [
+     CommonModule
+  ],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ height: 0, opacity: 1 }),
+            animate('0.3s ease-out',
+              style({ height: 400, opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ height: 400, opacity: 1 }),
+            animate('0.3s ease-in',
+              style({ height: 0, opacity: 1 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class AppComponent {
-  title = 'app';
+
+  _veranstaltungen: string[] = [];
+  @Input() set veranstaltungen(val: string) {
+    this._veranstaltungen = val.split(";");
+  }
+
+  _veranstaltungSelected: string | undefined;
+  @Input() set veranstaltung(val: string) {
+    this._veranstaltungSelected = val;
+  }
+
+  @Input() recaptchaSiteKey: string | undefined;
+
+  @Input() title = "Kaufen";
+
+  expanded = false;
 }
--- static/app/src/main.ts
+++ static/app/src/main.ts
@@ -1,6 +1,7 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { bootstrapApplication, createApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
@@ -9,17 +10,27 @@ if (environment.production) {
   enableProdMode();
 }
 
+if (!environment.production) {
+  console.log("ltw development")
+  // for development mode only
+  bootstrapApplication(AppComponent, {
+    providers: provideAnimations()
+  });
+}
+else {
+  // get a hand on the `ApplicationRef` to access its injector
+  createApplication({
+    providers: [
+      provideAnimations()
+    ]
+  }).then((appRef) => {
+    // create a constructor of a custom element
+    const buchung = createCustomElement(
+      AppComponent, // component for Angular element
+      { injector: appRef.injector } // used to inject the component to the DOM
+    );
 
-bootstrapApplication(AppComponent);
-
-// get a hand on the `ApplicationRef` to access its injector
-createApplication({ providers: [] }).then((appRef) => {
-  // create a constructor of a custom element
-  const buchung = createCustomElement(
-    AppComponent, // component for Angular element
-    { injector: appRef.injector } // used to inject the component to the DOM
-  );
-
-  // register in a browser
-  customElements.define('ifge-buchung', buchung);
-});
+    // register in a browser
+    customElements.define('ifge-buchung', buchung);
+  });
+}
