e9f2b74a031999682ff394deaa32eab5a0bcf930
Podlisca <67630279+Podlisca@users.noreply.github.com>
2025-08-19 14:17:43 +0200

    push

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,7 @@
 importScripts("https://cdn.brevo.com/js/sdk-loader.js");
 Brevo.push([
-    "init",
-    {
-        client_key: (location.search.match(/[?&]key=([^&]*)/) || [])[1],
-    },
+  "init",
+  {
+    client_key: "eh4n71if8r6ihyeb7lh6emg2"
+  }
 ]);
